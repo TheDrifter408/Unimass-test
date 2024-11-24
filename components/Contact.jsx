18c8dab5f2f50b172d60ad2b194a6cd3bc46786f
Noor Ullah Al Noor <nurullahan408@gmail.com>
2024-11-24 13:16:52 +0600

    updated the contact section to be responsive

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,17 +6,17 @@ import styled from "styled-components";
 export default function Contact(){
     return(
         <StyledSection>
-            <Container fluid="xs">
+            <Container fluid>
                 <Row>
-                    <Col className="p-0">
+                    <Col className="p-0" xs={12} md={5}>
                         <div className="formRight">
                             <div className="formRightInner">
-                                <h1>Stay connected with us!</h1>
+                                <h3>Stay connected with us!</h3>
                                 <h4>Get all the latest updates easily</h4>
                             </div>
                         </div>
                     </Col>
-                    <Col className="p-0">
+                    <Col className="p-0" xs={12} md={7}>
                         <div className="formLeft">
                             <div className="formLeftInner">
                                 <form>
@@ -36,32 +36,42 @@ export default function Contact(){
 const StyledSection = styled.section`
     .formRight {
         color:white;
-        padding-block: 30px;
+        padding: 30px 15px;
+        height:100%;
+        width:100%;
         background-color:rgb(26,71,148);
         .formRightInner {
             height:100%;
-            display:block;
+            display:grid;
+            gap:5px;
             margin-inline:auto;
             width:fit-content;
+            h3 {
+                font-weight: 800;
+            }
+            h4 {
+                font-size: 14px;
+            }
         }
     }
     .formLeft {
         color:white;
         background-color: rgb(82,95,110);
-        padding-block:30px;
+        padding:30px 15px;
         height:100%;
+        width:100%;
         .formLeftInner {
-            display:block;
             font-size:18px;
-            margin-block: 10px;
             form {
                 display:flex;
-                gap: 10px;
-                padding-left:20px;
+                flex-wrap: wrap;
+                align-items: center;
                 input {
+                    width:min(300px, 90vw);
                     background-color:transparent;
                     border-color: rgba(255, 255, 255,0.8);
                     border-top:0;
+                    margin: 10px;
                     border-inline: 0;
                     padding-bottom: 15px;
                     &:focus {
@@ -74,6 +84,9 @@ const StyledSection = styled.section`
                     background:transparent;
                     border-radius: 20px;
                     padding: 10px 20px;
+                    margin-left: 15px;
+                    flex-grow:0;
+                    flex-basis: 0;
                 }
             }
         }
